7ac6bf73d6f1e1524d912028d7244dcf1a8d4f96
edu <edu@edu-Inspiron-3583>
2023-10-13 08:38:57 -0300

    First Commit

---
+++ consultaShop/src/main/java/br/com/consultashop/bean/ProdutoBean.java
@@ -0,0 +1,32 @@
+package br.com.consultashop.bean;
+
+public class ProdutoBean {
+	private String descricaoProduto, codigoBarra, precoUnitario;
+	
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+	
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+	public String getCodigoBarra() {
+		return codigoBarra;
+	}
+
+	public void setCodigoBarra(String codigoBarra) {
+		this.codigoBarra = codigoBarra;
+	}
+
+	public String getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public void setPrecoUnitario(String precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+
+	
+	
+}
